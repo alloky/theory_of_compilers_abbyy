28fc46d91ad2f8e0855fc7a97b9ed68bf3fa98e1
Vitaly Ivanin <alloky2011@gmail.com>
2019-10-15 16:07:41 +0300

    try to add ambigious grammar

--- parser.py
+++ parser.py
@@ -17,33 +17,60 @@ class Node:
 class MiniJavaParser:
     tokens = MiniJavaLexer.tokens
     
-    def p_expression_plus(self, p):
-        'expression : expression PLUS term'
-        p[0] = Node('PLUS', p[1], p[3])
- 
-    def p_expression_minus(self, p):
-        'expression : expression MINUS term'
-        p[0] = Node('MINUS', p[1], p[3])
+    # def p_goal(self, p):
+    #     '''goal : mainclass LPAREN classdecl RPAREN'''
+
+    # def p_main(self, p):
+    #     '''mainclass : CLASS ID LPARBR PUBLIC STATIC VOID main LPAREN ID LPARSQ RPARSQ ID RPAREN LPARBR statement RPARBR'''
+
+    # def p_classdecl(self, p):
+    #     ''''classdecl : CLASS ID LPARBR vardecl methoddecl RPARBR 
+    #                   | CLASS ID EXTENDS ID LPARBR vardecl methoddecl RPARBR'''
+           
+    # def p_vardecl(self, p):
+    #     '''vardecl : type ID SEMICOL vardecl
+    #                | type ID SEMICOL'''
+
+    # def p_methoddecl(self, p):
+    #     '''methoddecl : PUBLIC type ID LPAREN argseq RPAREN LPARBR vardecl statement RETURN expression RPARBR
+    #                   | PRIVATE type ID LPAREN argseq RPAREN LPARBR vardecl statement RETURN expression RPARBR'''
+    
+    # def p_type(self, p):
+    #     '''type : INT LPARSQ RPARSQ
+    #             | INT
+    #             | BOOL
+    #             | ID'''
+    
+    # def p_statement(self, p):
+    #     '''statement : LPARBR statement RPARBR
+    #                  | IF LPAREN expression RPARBR statement else statement
+    #                  | WHILE LPAREN expression RPARBR statement
+    #                  | PRINT LPAREN expression RPARBR SEMICOL
+    #                  | ID ASSIGN expression SEMICOL
+    #                  | ID LPARBR expression RPARBR ASSIGN SEMICOL'''
+
+
+    def p_expression_plus_minus(self, p):
+        '''expression : expression PLUS term 
+                      | expression MINUS term'''
+        p[0] = Node(p[2], p[1], p[3])
  
     def p_expression_term(self, p):
         'expression : term'
         p[0] = p[1]
     
-    def p_term_times(self, p):
-        'term : term TIMES factor'
-        p[0] = Node('TIMES', p[1], p[3])
-    
-    def p_term_div(self, p):
-        'term : term DIVIDE factor'
-        p[0] = Node('DIVIDE', p[1], p[3])
-    
+    def p_term_times_div(self, p):
+        ''''term : term TIMES factor
+                 | term DIVIDE factor'''
+        p[0] = Node(p[2], p[1], p[3])
+
     def p_term_factor(self, p):
         'term : factor'
         p[0] = p[1]
     
     def p_factor_num(self, p):
         'factor : NUMBER'
-        p[0] = Node('NUMBER', None, None)
+        p[0] = Node(p[1], None, None)
         p[0].value = p[1]
     
     def p_factor_expr(self, p):
